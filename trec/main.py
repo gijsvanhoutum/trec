57a7b8de3c7e3cab567f6429d7c7ab1faf424ac1
gijsvanhoutum <gjjvanhoutum@gmail.com>
2023-12-20 15:40:42 -0500

    trec

--- trec/main.py
+++ trec/main.py
@@ -12,6 +12,36 @@ from display import DisplayHandler
 MAX_Q = 5
 
 class Model(QObject):    
+    """Fetches rows from a Smalltable.
+
+    Retrieves rows pertaining to the given keys from the Table instance
+    represented by table_handle.  String keys will be UTF-8 encoded.
+
+    Args:
+      table_handle:
+        An open smalltable.Table instance.
+      keys:
+        A sequence of strings representing the key of each table row to
+        fetch.  String keys will be UTF-8 encoded.
+      require_all_keys:
+        If True only rows with values set for all keys will be returned.
+
+    Returns:
+      A dict mapping keys to the corresponding table row data
+      fetched. Each row is represented as a tuple of strings. For
+      example:
+
+      {b'Serak': ('Rigel VII', 'Preparer'),
+       b'Zim': ('Irk', 'Invader'),
+       b'Lrrr': ('Omicron Persei 8', 'Emperor')}
+
+      Returned keys are always bytes.  If a key from the keys argument is
+      missing from the dictionary, then that row was not found in the
+      table (and require_all_keys must have been False).
+
+    Raises:
+      IOError: An error occurred accessing the smalltable.
+    """
     def __init__(self):                             
         super(self.__class__,self).__init__()        
 
